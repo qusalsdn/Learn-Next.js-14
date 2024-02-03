5569dbe36a32a2866725d80bc526e43b9ec61589
변민우 <yja05092@gmail.com>
2024-02-03 15:39:27 +0900

    SSR과 CSR 비교
    SSR은 서버에서 HTML을 생성하기 때문에 유저가 UI를 즉시 볼 수 있는 반면에 CSR은 브라우저에서 자바스크립트를 호출한 후 HTML을 생성하기 때문에 로딩중에는 UI를 볼 수 없다.

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,3 +1,4 @@
+// use client는 헷갈릴 수 있지만 CSR이 아니라 SSR로 렌더링을 하겠다고 선언하는 것이다.
 "use client";
 
 import Link from "next/link";
@@ -5,6 +6,8 @@ import { usePathname } from "next/navigation";
 
 export default function Navigation() {
   const path = usePathname();
+  // 콘솔을 찍어보면 벡엔드에도 콘솔이 찍히는 것을 볼 수 있다.
+  console.log("SSR");
 
   return (
     <nav>
