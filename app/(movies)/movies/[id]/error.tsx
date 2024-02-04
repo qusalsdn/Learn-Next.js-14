fb0abbba63d1dccf7fc742ae057c64e148dc125c
변민우 <yja05092@gmail.com>
2024-02-04 16:59:19 +0900

    Error Handling
    error.tsx 파일을 만들어주면 에러가 발생시 해당 파일의 UI를 보여주게 된다.

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Error() {
+  return <h1>lol something broke.....😥</h1>;
+}
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,8 +2,9 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
-  //   await new Promise((resolve) => setTimeout(resolve, 3000));
-  return fetch(`${API_URL}/${id}/videos`).then((res) => res.json());
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw Error("something broke...");
+  //   return fetch(`${API_URL}/${id}/videos`).then((res) => res.json());
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
