e2b6de795d2b37a5ef94a664467d9469ef61343e
변민우 <yja05092@gmail.com>
2024-02-04 16:09:25 +0900

    Parallel Requests
    Promise.all을 이용하면 배열 안에 있는 모든 것들을 동시에 시작하게 된다. 즉, 병렬 요청을 하게 된다.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,30 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+interface Movies {
+  id: number;
+  title: string;
+}
+
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  return fetch(URL).then((res) => res.json());
+  return fetch(API_URL).then((res) => res.json());
 }
 
 export default async function Home() {
-  const movies: any[] = await getMovies();
-  console.log(movies.length);
-  return <div>{JSON.stringify(movies)}</div>;
+  const movies: Movies[] = await getMovies();
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
