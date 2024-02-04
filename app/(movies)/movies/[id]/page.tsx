3dc2b28d83b359d7c382454a900dd495c71ac86d
변민우 <yja05092@gmail.com>
2024-02-04 16:36:37 +0900

    Suspense
    Promise.all을 이용하면 배열 안에 들어있는 작업들이 모두 끝나야만 UI를 보여주지만 Suspense를 이용하면 먼저 로딩이 끝나는 컴포넌트 별로 UI를 보여준다.

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,26 +1,16 @@
-import { API_URL } from "../../../(home)/page";
-
-interface Movie {
-  title: string;
-}
-
-async function getMovie(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  return fetch(`${API_URL}/${id}`).then((res) => res.json());
-}
-
-async function getVideos(id: string) {
-  console.log(`Fetching videos: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  return fetch(`${API_URL}/${id}/videos`).then((res) => res.json());
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
 
 export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
-  console.log("start fetching");
-  // Promise.all을 이용하면 배열 안에 있는 모든 것들을 동시에 시작하게 된다. 즉, 병렬 요청을 하게 된다.
-  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
-  console.log("end fetching");
-
-  return <h1>{movie.title}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
