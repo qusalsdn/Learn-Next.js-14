4559acd2e7cb9eef4659f89b33a748f8be2a1482
변민우 <yja05092@gmail.com>
2024-02-05 17:07:57 +0900

    export API_URL

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import Movie from "../../components/movie";
 import styles from "../../styles/home.module.css";
+import { API_URL } from "../contants";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -11,8 +12,6 @@ interface Movies {
   title: string;
   poster_path: string;
 }
-
-export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
   return fetch(API_URL).then((res) => res.json());
